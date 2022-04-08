aff9c7f7ddcdcad933203f422a607185d777a1b5
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:38 -0400

    Adding redislock to only process 1 at a time

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'coingate'
 gem 'aasm'
 gem 'sidekiq'
 gem 'ruby-vips'
+gem 'sidekiq-lock'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
--- app/workers/galaxy_worker.rb
+++ app/workers/galaxy_worker.rb
@@ -1,12 +1,27 @@
 class GalaxyWorker
   include Sidekiq::Worker
+  include Sidekiq::Lock::Worker
+
+  # static lock that expires after one second
+  sidekiq_options lock: { timeout: 20000, name: 'lock-worker' }
 
   def perform(item_id)
-    item = Item.find(item_id)
+    if lock.acquire!
+      begin
+        item = Item.find(item_id)
 
-    gif = Vips::Compile.new(item).gif
-    item.meme_card = gif
-    item.processing = false
-    item.save
+        gif = Vips::Compile.new(item).gif
+        item.meme_card = gif
+        item.processing = false
+        item.save!
+      ensure
+        # You probably want to manually release lock after work is done
+        # This method can be safely called even if lock wasn't acquired
+        # by current worker (thread). For more references see RedisLock class
+        lock.release!
+      end
+    else
+      raise "Could not acquire lock! Retrying!"
+    end
   end
 end
