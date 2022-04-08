fe9e0c40e11fefac359ba4908b23684aaa291a4b
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:36 -0400

    Simplify some of the gif compiling code

--- app/services/vips/compile.rb
+++ app/services/vips/compile.rb
@@ -4,36 +4,23 @@ module Vips
       @item = item
     end
 
+    def preview
+      page = template.crop(0, 0, template.width, page_height)  # Grab first image of gif
+      page = page.composite(resize_and_alpha, "over")          # Add picture
+      page = page.composite(rmagick_overlay, "over")           # Add text
+
+      StringIO.new(page.write_to_buffer ".png")
+    end
+
     def gif
-      image_binary = Paperclip.io_adapters.for(@item.image).read
-      meme_image = Vips::Image.new_from_buffer(image_binary, "")
-      target_size = 466 # Take from template
-
-      scale = [target_size.to_f / meme_image.width, target_size.to_f / meme_image.height].min
-      meme_image = meme_image.resize(scale, kernel: :cubic) # resize to fit a inside target_size by target_size box
-      rgba = meme_image.bandjoin(255)
-      x_coord = ((451 - rgba.width) / 2) + 86
-      y_coord = ((466 - rgba.height) / 2) + 188
-      rgba_embed = rgba.embed(x_coord, y_coord, 625, 1000)
-
-      gif = Vips::Image.new_from_file("data/pics/template_galaxy.gif", access: :sequential, n: -1)
-
-      # image.height will be for all frames -- we want the height of one page. If
-      # page-height is missing, default to image height
-      page_height = if gif.get_typeof("page-height") != 0
-                      gif.get("page-height")
-                    else
-                      gif.height
-                    end
-
-      # we now know the number of pages in the animation
-      n_loaded_pages = gif.height / page_height
+      n_loaded_pages = template.height / page_height
       pages = Array.new(n_loaded_pages)
       n_loaded_pages.times do |i|
-        pages[i] = gif.crop(0, i * page_height, gif.width, page_height)
+        pages[i] = template.crop(0, i * page_height, template.width, page_height)
       end
 
       overlay = rmagick_overlay
+      rgba_embed = resize_and_alpha
 
       pages = pages.map do |f|
         f = f.composite(rgba_embed, "over")
@@ -44,33 +31,47 @@ module Vips
       StringIO.new(newgif.write_to_buffer ".gif", optimize_gif_frames: true, optimize_gif_transparency: true, format: "gif")
     end
 
-    def preview
-      image_binary = Paperclip.io_adapters.for(@item.image).read
+    def resize_and_alpha
       meme_image = Vips::Image.new_from_buffer(image_binary, "")
-      target_size = meme_image.height > meme_image.width ? 466 : 451
 
+      target_size = meme_image.height > meme_image.width ? 466 : 451
       scale = [target_size.to_f / meme_image.width, target_size.to_f / meme_image.height].min
       meme_image = meme_image.resize(scale, kernel: :cubic) # resize to fit a inside target_size by target_size box
-      rgba = meme_image.bandjoin(255)
+
+      rgba = (meme_image.bands == 3) ? meme_image.bandjoin(255) : meme_image # Add alpha layer if 3 band image
       x_coord = ((451 - rgba.width) / 2) + 86
       y_coord = ((466 - rgba.height) / 2) + 188
-      rgba_embed = rgba.embed(x_coord, y_coord, 625, 1000)
+      rgba.embed(x_coord, y_coord, 625, 1000)
+    end
 
-      gif = Vips::Image.new_from_file("data/pics/template_galaxy.gif", access: :sequential, n: -1)
+    def is_gif?
+      @item.image_content_type == "image/gif"
+    end
 
+    def resize_and_alpha_gif
+      meme_gif = Vips::Image.gifload_buffer(image_binary, access: :sequential, n: -1)
+      page_height = meme_gif.get("page-height")
+      num_pages = meme_gif.height / page_height
+
+      target_size = page_height > meme_gif.width ? 466 : 451
+      scale = [target_size.to_f / meme_gif.width, target_size.to_f / page_height].min
+
+      # TODO - Does not update page-height. Perhaps break into individual frames, resize them each, and send back to method?
+      meme_gif.resize(scale, kernel: :cubic)
+    end
+
+    def image_binary
+      Paperclip.io_adapters.for(@item.image).read
+    end
+
+    def page_height
       # image.height will be for all frames -- we want the height of one page. If
       # page-height is missing, default to image height
-      page_height = if gif.get_typeof("page-height") != 0
-                      gif.get("page-height")
-                    else
-                      gif.height
-                    end
+      @page_height ||= template.get_typeof("page-height") != 0 ? template.get("page-height") : template.height
+    end
 
-      page = gif.crop(0, 0, gif.width, page_height) # Grab first image of gif
-      page = page.composite(rgba_embed, "over")      # Add picture
-      page = page.composite(rmagick_overlay, "over") # Add text
-
-      StringIO.new(page.write_to_buffer ".png")
+    def template
+      @template ||= Vips::Image.new_from_file("data/pics/template_galaxy.gif", access: :sequential, n: -1)
     end
 
     def rmagick_overlay
