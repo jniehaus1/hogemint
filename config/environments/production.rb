d5ec52cec9a92f5dab3c30e157341c7aff0cdca8
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:27 -0400

    Adding default url :host to rails production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  routes.default_url_options[:host] = "https://www.hogemint.com"
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
