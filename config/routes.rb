424c1ad867a4c4a8def547524c98998d3a766952
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:26 -0400

    Removing index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :test_items, only: [:new, :create, :show, :index]
-  resources :items, only: [:show, :index]
+  resources :items, only: [:show]
 
   get '/uri/:id', to: 'items#uri'
   get 'faq',      to: 'statics#faq'
