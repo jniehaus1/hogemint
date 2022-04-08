4ed2cf3b91e01feaa8af190f32f7e22f2e36d72f
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:36 -0400

    Adding CSV of holders for galaxy

--- config/initializers/eth_addresses.rb
+++ config/initializers/eth_addresses.rb
@@ -5,6 +5,7 @@ def HexConverter.convert(value)
   value.hex
 end
 
-HOGE_HOLDERS = SmarterCSV.process("data/hoge_holders.csv", value_converters: { holderaddress: HexConverter }).pluck(:holderaddress).sort
-HOGE_HOLDERS_EXPANSION = SmarterCSV.process("data/hoge_holders_expansion.csv", value_converters: { holderaddress: HexConverter }).pluck(:holderaddress).sort
+# HOGE_HOLDERS = SmarterCSV.process("data/hoge_holders.csv", value_converters: { holderaddress: HexConverter }).pluck(:holderaddress).sort
+# HOGE_HOLDERS_EXPANSION = SmarterCSV.process("data/hoge_holders_expansion.csv", value_converters: { holderaddress: HexConverter }).pluck(:holderaddress).sort
 HOGE_HOLDERS_GALAXY = SmarterCSV.process("data/hoge_holders_galaxy.csv", value_converters: { holderaddress: HexConverter }).pluck(:holderaddress).sort
+HOGE_HOLDERS_GALAXY.append(SmarterCSV.process("data/hoge_holders_galaxy_bsc.csv", value_converters: { holderaddress: HexConverter }).pluck(:holderaddress).sort)
