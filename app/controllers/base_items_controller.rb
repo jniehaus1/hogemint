6666878bff8ab2bb17285aea72ba96fea9ab48aa
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:27 -0400

    Adding login view and #new pathway for base_items

--- app/controllers/base_items_controller.rb
+++ app/controllers/base_items_controller.rb
@@ -1,13 +1,34 @@
 class BaseItemsController < ApplicationController
-  before_action :check_session_pw
+  before_action :check_session_pw, except: [:new_session, :login]
 
   # I know it's bad, and I just don't care right now.
   # Will implement devise in the future.
   def login
-
+    if params[:password] == ENV["SESSION_PW"]
+      session[:pw] = "sexy-beast"
+      redirect_to new_base_item_url
+    else
+      redirect_to "new_session"
+    end
   end
 
+  def new_session; end
+
+  def new
+    @base_item = BaseItem.new
+  end
+
+  def create; end
+
+  def show; end
+
   def check_session_pw
-    redirect_to login if session[:pw] != "sexy-beast"
+    redirect_to :new_session if session[:pw] != "sexy-beast"
+  end
+
+  private
+
+  def base_item_params
+    params.require(:base_item).permit(:owner, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :test_items, only: [:new, :create, :show, :index]
-  resources :items, only: [:show]
+  resources :test_items, only: [:new, :create, :show]
+  resources :items,      only: [:show]
+  resources :base_items, only: [:new, :create, :show]
 
   get '/uri/:id', to: 'items#uri'
+
+  get  '/new_session', to: 'base_items#new_session'
+  post '/login',       to: 'base_items#login'
+
   get 'faq',      to: 'statics#faq'
   get 'guide',    to: 'statics#guide'
 
