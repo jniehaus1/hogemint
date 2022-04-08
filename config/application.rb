c13c092d29d66ec7ac9b7c8d064fc2c7cd8b057e
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:25 -0400

    Adding s3 to paperclip

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module Hogemint
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    #
+    config.paperclip_defaults = {
+        storage: :s3,
+        s3_region: ENV["AWS_S3_REGION"],
+        s3_permissions: "public-read",
+        s3_credentials: {
+            s3_host_name: ENV["AWS_S3_HOST_NAME"],
+            bucket: ENV["AWS_S3_BUCKET"],
+            access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+            secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+        }
+    }
   end
 end
