c0e5321da920995f8f409663f53141aab2d962a4
Joseph Niehaus <jdniehaus@gmail.com>
2022-04-07 20:49:32 -0400

    Bad fork of nftprinter::create to handle minting gen1 on different ABI

---
+++ app/services/nft_printer/create_gen_one.rb
@@ -0,0 +1,55 @@
+module NftPrinter
+  class CreateGenOne
+    include ApplicationService
+
+    def initialize(item, owner, mint_addr, private_key)
+      @item  = item
+      @owner = owner
+      @mint_address = mint_addr
+      @private_key_str = private_key
+    end
+
+    def call
+      validate_inputs
+      validate_uri
+
+      contract = build_contract
+      contract.transact_and_wait.mint(@owner, @item.uri)
+    end
+
+    def build_contract
+      contract = Ethereum::Contract.create(name: "HogeNFT", address: @mint_address, abi: abi, client: client)
+      contract.key = private_key
+      contract
+    end
+
+    def private_key
+      @private_key ||= Eth::Key.new(priv: @private_key_str)
+    end
+
+    def abi
+      f = File.read("data/eth/HogeNFT.json");
+      data_hash = JSON.parse(f);
+      raise "Cannot find abi data from file" if data_hash["abi"].blank?
+      data_hash["abi"]
+    end
+
+    def client
+      @client = Ethereum::HttpClient.new(ENV["ETH_NODE"])
+      @client.gas_limit = ENV["MINT_GAS_LIMIT"].to_i # In Gas: 250000 for example
+      @client.gas_price = Etherscan::GasStation.gas_price * 1000000000 # in Wei: 110000000000 for example
+      @client
+    end
+
+    def validate_inputs
+      raise "No Owner Address" if @owner.blank?
+      raise "Nothing to Mint, Item doesn't exist" if @item.blank?
+      raise "No CONTRACT_ADDRESS" if @mint_address.blank?
+      raise "No MINT_PRIVATE_KEY" if @private_key_str.blank?
+    end
+
+    def validate_uri
+      raise "Bad URI at ID: #{@item.id} URI: #{@item.uri}" unless @item.uri.match?(/\h{32}/)
+    end
+  end
+end
--- app/workers/mint_worker.rb
+++ app/workers/mint_worker.rb
@@ -6,7 +6,13 @@ class MintWorker
     sale.mint! # Advance sale state
 
     nft_asset = sale.nft_asset
-    NftPrinter::Create.call(nft_asset, sale.nft_owner, mint_address(generation), private_key(generation))
+    if generation == "two"
+      NftPrinter::Create.call(nft_asset, sale.nft_owner, mint_address(generation), private_key(generation))
+    elsif generation == "one"
+      NftPrinter::CreateGenOne.call(nft_asset, sale.nft_owner, mint_address(generation), private_key(generation))
+    else
+      raise "Cannot mint, not generation 1 or 2."
+    end
 
     sale.finish_minting!
   rescue StandardError => e
